7b7d909f7e30b09f32ee61df8c0bea06cac75c62
arizz96 <arizz96@gmail.com>
2019-02-25 16:18:55 +0100

    Fixes

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@ echo "`date` ~~~~~~~~~~~ STARTING BACKUP ~~~~~~~~~~~~"
 rm -f /tmp/backup.native.dump.bz2
 echo "`date` Creating clickhouse dump"
 
-CMD="clickhouse-client --host ${CHHOST} --query=\"SELECT * FROM ${CHTABLE} FORMAT Native\""
-$BACKUP_PRIORITY $CMD > /tmp/backup.native.dump
+set -f
+$BACKUP_PRIORITY clickhouse-client --host $CHHOST --query="SELECT * FROM $CHTABLE FORMAT Native" > /tmp/backup.native.dump
 echo "`date` Compressing dump"
 $BACKUP_PRIORITY bzip2 /tmp/backup.native.dump
 
